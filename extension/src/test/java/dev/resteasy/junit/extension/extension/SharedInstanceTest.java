263ceaaf12a31529860cf492337894d8844c5dff
James R. Perkins <jperkins@redhat.com>
2024-07-29 08:11:46 -0700

    Check the identity of the injected Configuration to ensure it's the same.
    Signed-off-by: James R. Perkins <jperkins@redhat.com>

--- extension/src/test/java/dev/resteasy/junit/extension/extension/SharedInstanceTest.java
+++ extension/src/test/java/dev/resteasy/junit/extension/extension/SharedInstanceTest.java
@@ -48,19 +48,28 @@ public class SharedInstanceTest {
     @Test
     public void checkParameter(final Configuration configuration) {
         Assertions.assertNotNull(configuration);
-        Assertions.assertEquals(CONFIGURATION.get(), configuration);
-        Assertions.assertEquals(STATIC_CONFIGURATION, configuration);
-        Assertions.assertEquals(instanceConfiguration, configuration);
-        Assertions.assertEquals(constructorConfiguration, configuration);
+        checkInstance(CONFIGURATION.get(), configuration);
+        checkInstance(STATIC_CONFIGURATION, configuration);
+        checkInstance(instanceConfiguration, configuration);
+        checkInstance(constructorConfiguration, configuration);
     }
 
     @Test
     public void checkInjectedInstances() {
         Assertions.assertNotNull(constructorConfiguration);
         final var configuration = CONFIGURATION.get();
-        Assertions.assertEquals(STATIC_CONFIGURATION, configuration);
-        Assertions.assertEquals(instanceConfiguration, configuration);
-        Assertions.assertEquals(constructorConfiguration, configuration);
+        checkInstance(STATIC_CONFIGURATION, configuration);
+        checkInstance(instanceConfiguration, configuration);
+        checkInstance(constructorConfiguration, configuration);
 
+    }
+
+    private void checkInstance(final Configuration config1, final Configuration config2) {
+        Assertions.assertEquals(config1, config2,
+                () -> String.format("Configuration %s does not equal configuration %s", config1, config2));
+        // Check the objects identity are equal
+        Assertions.assertEquals(System.identityHashCode(config1), System.identityHashCode(config2), () -> String.format(
+                "The identity hash code of %d for configuration %s does not match the identity hash code of %d for configuration %s",
+                System.identityHashCode(config1), config1, System.identityHashCode(config2), config2));
     }
 }
