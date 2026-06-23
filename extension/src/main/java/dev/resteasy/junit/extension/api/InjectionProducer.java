a85db4f50e5b2151f08c6c78c27292f82558925d
James R. Perkins <jperkins@ibm.com>
2026-06-23 14:42:17 -0700

    Update the Javadoc to more user friendly.
    Signed-off-by: James R. Perkins <jperkins@ibm.com>

--- extension/src/main/java/dev/resteasy/junit/extension/api/InjectionProducer.java
+++ extension/src/main/java/dev/resteasy/junit/extension/api/InjectionProducer.java
@@ -10,34 +10,120 @@ import java.lang.annotation.Annotation;
 import org.junit.jupiter.api.extension.ExtensionContext;
 
 /**
- * An implementation of this interface is used to inject static fields, instance fields and parameters. For static
- * fields and instance fields, the field must be annotated with {@link jakarta.inject.Inject}.
+ * Enables injection of custom types into test classes.
+ * <p>
+ * Implementations of this interface can provide custom objects for injection into test fields (static or instance) and
+ * method parameters (constructor, lifecycle methods, or test methods). All injectable fields and parameters must be
+ * annotated with {@link dev.resteasy.junit.extension.annotations.RestResource @RestResource}.
+ * </p>
+ *
+ * <h2>Built-in Producers</h2>
+ * <p>
+ * The extension provides built-in producers for:
+ * </p>
+ * <ul>
+ * <li>{@link jakarta.ws.rs.SeBootstrap.Configuration} - The SeBootstrap configuration</li>
+ * <li>{@link jakarta.ws.rs.client.Client} - REST client instance</li>
+ * <li>{@link jakarta.ws.rs.client.WebTarget} - WebTarget (optionally qualified with {@code @RequestPath})</li>
+ * <li>{@link jakarta.ws.rs.core.UriBuilder} - URI builder</li>
+ * <li>{@link java.net.URI} - Base URI (optionally qualified with {@code @RequestPath})</li>
+ * </ul>
+ *
+ * <h2>Custom Producer Example</h2>
+ * <p>
+ * To inject custom types, implement this interface and register via {@link java.util.ServiceLoader}:
+ * </p>
+ *
+ * <pre>
+ * public class DataSourceProducer implements InjectionProducer {
+ *     &#64;Override
+ *     public boolean canInject(ExtensionContext context, Class&lt;?&gt; clazz, Annotation... qualifiers) {
+ *         return DataSource.class.isAssignableFrom(clazz);
+ *     }
+ *
+ *     &#64;Override
+ *     public Object produce(ExtensionContext context, Class&lt;?&gt; clazz, Annotation... qualifiers) {
+ *         if (!canInject(context, clazz, qualifiers)) {
+ *             throw new IllegalArgumentException("Cannot produce type: " + clazz);
+ *         }
+ *         // Create and return your DataSource instance
+ *         return createDataSource();
+ *     }
+ *
+ *     private DataSource createDataSource() {
+ *         // Implementation details...
+ *     }
+ * }
+ *
+ * // Now you can inject DataSource in tests:
+ * &#64;RestBootstrap(MyApp.class)
+ * public class MyTest {
+ *     &#64;RestResource
+ *     private DataSource dataSource;
+ *
+ *     &#64;Test
+ *     public void testDatabase() {
+ *         // Use dataSource...
+ *     }
+ * }
+ * </pre>
+ *
+ * <h2>Registration</h2>
+ * <p>
+ * Custom producers must be registered via {@link java.util.ServiceLoader} by creating a file
+ * {@code META-INF/services/dev.resteasy.junit.extension.api.InjectionProducer} containing the fully-qualified class
+ * name of your implementation.
+ * </p>
+ *
+ * <h2>Lifecycle</h2>
+ * <p>
+ * If the produced object implements {@link AutoCloseable}, it will be automatically closed when the test context ends.
+ * </p>
  *
  * @author <a href="mailto:jperkins@redhat.com">James R. Perkins</a>
+ * @see dev.resteasy.junit.extension.annotations.RestResource
+ * @since 1.0.0
  */
 public interface InjectionProducer {
 
     /**
-     * Indicates the type can be produced from this producer.
+     * Determines whether this producer can inject the specified type.
+     * <p>
+     * This method is called by the extension to find the appropriate producer for each injection point. It should
+     * return {@code true} only if this producer can create instances of the given type, optionally considering any
+     * qualifiers present.
+     * </p>
      *
-     * @param context    the current extension context
-     * @param clazz      the type to check
-     * @param qualifiers the qualifiers, if any, for the field or parameter
+     * @param context    the current JUnit extension context, provides access to the test class and instance
+     * @param clazz      the type being requested for injection (field type or parameter type)
+     * @param qualifiers annotation qualifiers present on the injection point (e.g., {@code @RequestPath},
+     *                   {@code @RestClientConfig})
      *
-     * @return {@code true} if this producer can create this type, otherwise {@code false}
+     * @return {@code true} if this producer can create instances of the given type, {@code false} otherwise
      */
     boolean canInject(ExtensionContext context, Class<?> clazz, Annotation... qualifiers);
 
     /**
-     * Creates the object which can be injected into a static field, an instance field or a parameter.
+     * Produces an instance of the requested type for injection.
+     * <p>
+     * This method is called when {@link #canInject(ExtensionContext, Class, Annotation...)} returns {@code true} for
+     * the given type. It should create and return an appropriate instance. If the returned object implements
+     * {@link AutoCloseable}, it will be automatically closed when the appropriate test context ends.
+     * </p>
+     * <p>
+     * Implementations should validate that they can produce the requested type and throw
+     * {@link IllegalArgumentException} if called with an unsupported type.
+     * </p>
      *
-     * @param context    the current extension context
-     * @param clazz      the type of the field or parameter
-     * @param qualifiers the qualifiers, if any, for the field or parameter
+     * @param context    the current JUnit extension context, provides access to the test class and instance
+     * @param clazz      the type of the field or parameter to inject
+     * @param qualifiers annotation qualifiers present on the injection point (e.g., {@code @RequestPath},
+     *                   {@code @RestClientConfig})
      *
-     * @return the newly constructed object or {@code null} if it could not be produced
+     * @return the newly created object to be injected, or {@code null} if it could not be produced
      *
-     * @throws IllegalArgumentException if the type cannot be assigned to the type this producer can produce
+     * @throws IllegalArgumentException if the type cannot be produced by this producer (implementations should validate
+     *                                  via {@link #canInject} first)
      */
     Object produce(ExtensionContext context, Class<?> clazz, Annotation... qualifiers) throws IllegalArgumentException;
 }
--- extension/src/main/java/dev/resteasy/junit/extension/api/RestClientBuilderProvider.java
+++ extension/src/main/java/dev/resteasy/junit/extension/api/RestClientBuilderProvider.java
@@ -10,24 +10,77 @@ import java.util.ServiceLoader;
 import jakarta.ws.rs.client.ClientBuilder;
 
 /**
- * A provider for creating the configuration for an injected {@linkplain jakarta.ws.rs.client.Client REST client}. The
- * {@link ClientBuilder} provided by this provider will be used to create the
- * {@linkplain jakarta.ws.rs.client.Client REST client}.
+ * Provides custom {@link jakarta.ws.rs.client.ClientBuilder} configuration for creating REST clients.
  * <p>
- * By default, this provider attempts to use a {@link java.util.ServiceLoader} to lookup the first provider found. If
- * found, that provider will be used. This can be useful when when you want to use the same provider across all tests
- * without having to define the type on each annotation.
+ * Implementations of this interface can customize how {@link jakarta.ws.rs.client.Client Client} instances are created
+ * for tests. This allows configuration of timeouts, SSL settings, custom providers, filters, and other client options.
  * </p>
  *
+ * <h2>Usage</h2>
+ * <p>
+ * Specify a custom provider using the {@link dev.resteasy.junit.extension.annotations.RestClientConfig @RestClientConfig}
+ * qualifier on injected Client or WebTarget fields/parameters:
+ * </p>
+ *
+ * <pre>
+ * public class CustomTimeoutProvider implements RestClientBuilderProvider {
+ *     &#64;Override
+ *     public ClientBuilder getClientBuilder() {
+ *         return ClientBuilder.newBuilder()
+ *                 .connectTimeout(5, TimeUnit.SECONDS)
+ *                 .readTimeout(30, TimeUnit.SECONDS);
+ *     }
+ * }
+ *
+ * &#64;RestBootstrap(MyApp.class)
+ * public class MyTest {
+ *     &#64;RestResource
+ *     &#64;RestClientConfig(CustomTimeoutProvider.class)
+ *     private Client client; // Will use custom timeouts
+ *
+ *     &#64;RestResource
+ *     private Client defaultClient; // Will use default configuration
+ * }
+ * </pre>
+ *
+ * <h2>Global Provider via ServiceLoader</h2>
+ * <p>
+ * Instead of specifying the provider on each injection point, you can register a global provider via
+ * {@link java.util.ServiceLoader}. Create a file
+ * {@code META-INF/services/dev.resteasy.junit.extension.api.RestClientBuilderProvider}
+ * containing your implementation class name.
+ * </p>
+ * <p>
+ * When a global provider is registered and no explicit {@code @RestClientConfig} is specified, the global provider will
+ * be used automatically for all injected Client instances.
+ * </p>
+ *
+ * <h2>Requirements</h2>
+ * <ul>
+ * <li>Must have a public no-argument constructor</li>
+ * <li>The {@link #getClientBuilder()} method must return a non-null ClientBuilder</li>
+ * <li>Can be registered globally via ServiceLoader or specified per-injection via {@code @RestClientConfig}</li>
+ * </ul>
+ *
  * @author <a href="mailto:jperkins@redhat.com">James R. Perkins</a>
+ * @see dev.resteasy.junit.extension.annotations.RestClientConfig
+ * @since 1.0.0
  */
 public interface RestClientBuilderProvider {
 
     /**
-     * A provider which can supply a {@link ClientBuilder} for create the
-     * {@linkplain jakarta.ws.rs.client.Client REST client}.
+     * Provides a {@link ClientBuilder} for creating {@linkplain jakarta.ws.rs.client.Client REST client} instances.
+     * <p>
+     * The default implementation checks for a globally registered provider via {@link ServiceLoader}. If found, that
+     * provider's ClientBuilder is used. Otherwise, returns the default builder created by
+     * {@link ClientBuilder#newBuilder()}.
+     * </p>
+     * <p>
+     * Implementations should override this method to provide custom client configuration such as timeouts, SSL settings,
+     * providers, or interceptors.
+     * </p>
      *
-     * @return the {@link ClientBuilder} to use for creating the REST client, cannot be {@code null}
+     * @return the {@link ClientBuilder} to use for creating REST client instances, must not be {@code null}
      */
     default ClientBuilder getClientBuilder() {
         final ServiceLoader<RestClientBuilderProvider> loader = ServiceLoader.load(RestClientBuilderProvider.class);
